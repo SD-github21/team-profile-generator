cd89e738480de33172cb54d00946978abfa63269
SD-github21 <sapandonde.gmail.com>
2022-05-19 21:42:28 -0500

    add failing test to create employee class

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,13 @@
+const Employee = require('../lib/Employee.js');
+
+test('creates an employee class', () => {
+    const employee = new Employee('Jarred');
+
+    expect(employee.name).toBe('Jarred');
+    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.email).toEqual(expect.stringContaining('com'));
+
+    expect(employee.getName()).toEqual(employee.name);
+    expect(employee.getId()).toEqual(employee.id);
+    expect(employee.getEmail()).toEqual(employee.email);
+});
