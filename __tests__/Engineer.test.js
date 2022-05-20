2c8c2a66d5a2e69f79187a45667f4c38ea5579a6
SD-github21 <sapandonde.gmail.com>
2022-05-19 23:04:52 -0500

    add failing test for Engineer child object and empty Engineer file

---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,19 @@
+const Engineer = require('../lib/Engineer.js');
+
+test('creates an engineer child object', () => {
+    const engineer = new Engineer('Sally', 2, 'sally@gmail.com', 'Engineer', 25);
+
+    expect(engineer.name).toBe('Sally');
+    expect(engineer.id).toEqual(expect.any(Number));
+    expect(engineer.email).toEqual(expect.stringContaining('com'));
+
+    expect(engineer.getName()).toEqual(engineer.name);
+    expect(engineer.getId()).toEqual(engineer.id);
+    expect(engineer.getEmail()).toEqual(engineer.email);
+
+    expect(engineer.getRole()).toEqual('Engineer');
+
+    expect(engineer.gitHub).toEqual(engineer.gitHub);
+});
+
+module.exports = Engineer;
