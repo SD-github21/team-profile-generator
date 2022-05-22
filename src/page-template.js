18c610cfdab0cc89399fa54d80a6938723230042
SD-github21 <sapandonde.gmail.com>
2022-05-22 01:59:15 -0500

    added revised code to successfully render HTML page with data

--- src/page-template.js
+++ src/page-template.js
@@ -1,46 +1,52 @@
-const generateHTML = teamDataArr => {
-    const manager = teamDataArr.filter(member => member.role === 'Manager');
-    const engineer = teamDataArr.filter(member => member.role === 'Engineer');
-    const intern = teamDataArr.filter(member => member.role === 'Intern');
-    console.log(manager);
-    console.log(engineer);
-    console.log(intern);
+const createProfiles = teamDataArr => {
+    const managers = teamDataArr.filter(member => member.role === 'Manager');
+    const engineers = teamDataArr.filter(member => member.role === 'Engineer');
+    const interns = teamDataArr.filter(member => member.role === 'Intern');
+    console.log(managers);
+    console.log(engineers);
+    console.log(interns);
     return `       
-        <div class="col-12 col-md-4 card">
-        ${manager
-        .map(({ name, id, role, email, officeNumber }) => {
+
+        ${managers
+        .map(manager => {
             return `
-            <h3 class="team-name">${name}</h3>
-            <h4 class="team-role">${role}</h4>
-            <p>ID: ${id}</p>
-            <p>Email: <a href = "mailto: ${email}"></a><p>
-            <p>Office number: ${officeNumber}
+            <div class="col-12 card">
+                <h3 class="team-name">${manager.getName()}</h3>
+                <h4 class="team-role">${manager.getRole()}</h4>
+                <p>ID: ${manager.getId()}</p>
+                <p>Email: <a href = "mailto: ${manager.getEmail()}"></a><p>
+                <p>Office number: ${manager.getOfficeNumber()}
+            </div>
         `;
        
         })
         .join("")}
 
-        ${engineer
-            .map(({ name, id, email, role, gitHub }) => {
+        ${engineers
+            .map(engineer => {
                 return `
-                <h3 class="team-name">${name}</h3>
-                <h4 class="team-role">${role}</h4>
-                <p>ID: ${id}</p>
-                <p>Email: <a href = "mailto: ${email}"></a><p>
-                <p>GitHub: ${gitHub}
+            <div class="col-12 card">
+                <h3 class="team-name">${engineer.getName()}</h3>
+                <h4 class="team-role">${engineer.getRole()}</h4>
+                <p>ID: ${engineer.getId()}</p>
+                <p>Email: <a href = "mailto: ${engineer.getEmail()}"></a><p>
+                <p>GitHub: ${engineer.getGitHub()}
+            </div>
             `;
            
             })
             .join("")}
     
-            ${intern
-                .map(({ name, id, email, role, school }) => {
+            ${interns
+                .map(intern => {
                     return `
-                    <h3 class="team-name">${name}</h3>
-                    <h4 class="team-role">${role}</h4>
-                    <p>ID: ${id}</p>
-                    <p>Email: <a href = "mailto: ${email}"></a><p>
-                    <p>School: ${school}
+                <div class="col-12 card">
+                    <h3 class="team-name">${intern.getName()}</h3>
+                    <h4 class="team-role">${intern.getRole()}</h4>
+                    <p>ID: ${intern.getId()}</p>
+                    <p>Email: <a href = "mailto: ${intern.getEmail()}"></a><p>
+                    <p>School: ${intern.getSchool()}
+                </div>
                 `;
                
                 })
@@ -52,45 +58,45 @@ const generateHTML = teamDataArr => {
  
 };
 
-module.exports = generateHTML;
+// module.exports = generateHTML;
 
 
-// // Export function to generate entire page
-// module.exports = templateData => {
+// Export function to generate entire page
+module.exports = templateData => {
 
-//     return `
-//     <!DOCTYPE html>
-//     <html lang="en">
+    return `
+    <!DOCTYPE html>
+    <html lang="en">
 
-//     <head>
-//         <meta charset="UTF-8">
-//         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-//         <meta http-equiv="X-UA-Compatible" content="ie-edge">
-//         <title>Team Profile Generator</title>
-//         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css">
-//         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
-//         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@300&display=swap">        
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie-edge">
+        <title>Team Profile Generator</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@300&display=swap">        
 
-//         <link rel="stylesheet" href="../dist/style.css">
-//     <head>
+        <link rel="stylesheet" href="../dist/style.css">
+    <head>
 
-//     <body>
-//         <div class="row">
-//             <div class="col-12 w-100">
-//                 <header>
-//                     <h1 class="team-title">My Team</h1>
-//                </header>            
-//             </div>
-//         </div>
+    <body>
+        <div class="row">
+            <div class="col-12 w-100">
+                <header>
+                    <h1 class="team-title">My Team</h1>
+               </header>            
+            </div>
+        </div>
 
 
-//         <main class="container">
-//             <section class="my-3" id="team-members">
-//                 ${createProfiles(teamData)} 
+        <main class="container">
+            <section class="my-3" id="team-members">
+                ${createProfiles(templateData)} 
 
-//         </main>
+        </main>
 
-//     </body>
-//     </html>
-//       `;
-// };
+    </body>
+    </html>
+      `;
+};
